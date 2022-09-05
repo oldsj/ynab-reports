469ba98e2b8cf557348897e688f096857dd04e76
James Olds <james@oldsmail.com>
2022-09-05 11:01:12 -0400

    send plot directly with redmail

--- graph.py
+++ graph.py
@@ -1,17 +1,46 @@
+from xmlrpc.client import ProtocolError
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
- 
+
+from mail import send_report
+
+fig = plt.figure()
 # Data
-df=pd.DataFrame({'x_values': range(1,11), 'y1_values': np.random.randn(10), 'y2_values': np.random.randn(10)+range(1,11), 'y3_values': np.random.randn(10)+range(11,21) })
- 
+df = pd.DataFrame(
+    {
+        "x_values": range(1, 11),
+        "y1_values": np.random.randn(10),
+        "y2_values": np.random.randn(10) + range(1, 11),
+        "y3_values": np.random.randn(10) + range(11, 21),
+    }
+)
+
 # multiple line plots
-plt.plot( 'x_values', 'y1_values', data=df, marker='o', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=4)
-plt.plot( 'x_values', 'y2_values', data=df, marker='', color='olive', linewidth=2)
-plt.plot( 'x_values', 'y3_values', data=df, marker='', color='olive', linewidth=2, linestyle='dashed', label="toto")
+plt.plot(
+    "x_values",
+    "y1_values",
+    data=df,
+    marker="o",
+    markerfacecolor="blue",
+    markersize=12,
+    color="skyblue",
+    linewidth=4,
+)
+plt.plot("x_values", "y2_values", data=df, marker="", color="olive", linewidth=2)
+plt.plot(
+    "x_values",
+    "y3_values",
+    data=df,
+    marker="",
+    color="olive",
+    linewidth=2,
+    linestyle="dashed",
+    label="toto",
+)
 
 # show legend
 plt.legend()
 # plt.show()
 
-plt.savefig("report.png")
+send_report(fig)
--- mail.py
+++ mail.py
@@ -1,12 +1,30 @@
 import os
-import yagmail
+from redmail import EmailSender
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# send email
 recipients = os.getenv("MAIL_RECIPIENTS").split(",")
 mail_user = os.getenv("MAIL_USERNAME")
 mail_password = os.getenv("MAIL_PASSWORD")
+mail_host = os.getenv("MAIL_SERVER")
+mail_port = os.getenv("MAIL_PORT")
+mail_sender = os.getenv("MAIL_DEFAULT_SENDER")
 
-yag = yagmail.SMTP(mail_user, mail_password)
-yag.send(to=recipients, subject="YNAB Report", contents=[yagmail.inline("report.png")])
+email = EmailSender(
+    host=mail_host, port=mail_port, username=mail_user, password=mail_password
+)
+email.receivers = recipients
+email.set_template_paths(html=".")
+
+
+def send_report(fig):
+    email.send(
+        sender=f"{mail_sender} <{mail_user}>",
+        subject="YNAB Report",
+        html_template="report.html.j2",
+        body_images={
+            "report_plot": fig,
+        },
+    )
